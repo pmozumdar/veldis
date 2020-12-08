08cc4f660d9fa44c5d5f34fc9f8a4c47df28fcb6
Pritom Mozumdar <prit0m0806082@gmail.com>
2020-12-08 09:36:22 -0800

    don't know the outcome of this commit

--- gaussian_fit.py
+++
@@ -1,8 +0,0 @@
-"""
-gausssian_fit.py
-
-This file contains the 'Gaussfit' class to fit sky lines of the 
-spectra to Gaussian profiles in order to calculate sigma of the 
-instrument's LSF(line of sight).
-"""
------------------------------------------------------------------------
